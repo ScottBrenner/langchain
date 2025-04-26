a9e918a65703e74fde895b62a2ad320a595b2aa3
Scott Brenner <scott@scottbrenner.me>
2025-04-25 20:02:56 -0700

    Update .github/workflows/extract_ignored_words_list.py

--- .github/workflows/extract_ignored_words_list.py
+++ .github/workflows/extract_ignored_words_list.py
@@ -7,4 +7,4 @@ ignore_words_list = (
     pyproject_toml.get("tool", {}).get("codespell", {}).get("ignore-words-list")
 )
 
-print(list(ignore_words_list))
+print(ignore_words_list=$ignore_words_list)
